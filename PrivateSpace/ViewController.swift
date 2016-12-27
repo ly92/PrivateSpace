af1d7db906d384c55c41d05fa46f66d5e1e487a9
ly <1364757394@qq.com>
2016-12-27 23:05:03 +0800

    Initial Commit

---
+++ PrivateSpace/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PrivateSpace
+//
+//  Created by 李勇 on 2016/12/27.
+//  Copyright © 2016年 ly. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
